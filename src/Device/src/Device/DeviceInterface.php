c02ad63eee7fcd1e9128912a9b4d88f685a23f1c
Pieter Kokx <pieter@kokx.nl>
2019-04-13 22:53:15 +0200

    Added initial configuration example

---
+++ src/Device/src/Device/DeviceInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Device\Device;
+
+interface DeviceInterface
+{
+
+}
---
+++ src/Device/src/Handler/OpenThermGateway.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Device\Handler;
+
+use Device\Device\DeviceInterface;
+
+/**
+ * Device based on the OpenTherm Gateway, by Schelte Bron.
+ *
+ * @link http://otgw.tclcode.com/ The OpenTherm Gateway
+ */
+class OpenThermGateway implements DeviceInterface
+{
+
+}
