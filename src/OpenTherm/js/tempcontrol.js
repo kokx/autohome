3f520d38b77b9aeb5fa74ea6ef2ebdd745d3e55c
Pieter Kokx <pieter@kokx.nl>
2019-04-21 18:02:11 +0200

    Finished component for temperature control

--- src/OpenTherm/js/tempcontrol.js
+++ src/OpenTherm/js/tempcontrol.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 const TempControl = props => {
-  console.log(props.state);
-  return <div className="input-group">
-            <div className="input-group-prepend">
-              <button className="btn btn-outline-secondary">-</button>
-            </div>
-              <input id="actuator_state" className="form-control" name="state" type="number" value="32.00" step="0.5"/>
-            <div className="input-group-append">
-              <button className="btn btn-outline-secondary">+</button>
-            </div>
-          </div>;
+  const state = parseInt(props.state, 10);
+  const [temp, setTemperature] = useState(state);
+  return (
+    <div className="input-group">
+      <div className="input-group-prepend">
+        <button
+          type="button"
+          className="btn btn-outline-secondary"
+          onClick={() => setTemperature(temp - 0.5)}>-</button>
+      </div>
+      <input
+        id="actuator_state"
+        className="form-control"
+        name="state"
+        type="number"
+        value={temp}
+        onChange={(e) => setTemperature(e.target.value)}
+        step="0.5"/>
+      <div className="input-group-append">
+        <button
+          type="button"
+          className="btn btn-outline-secondary"
+          onClick={() => setTemperature(temp + 0.5)}>+</button>
+      </div>
+    </div>
+  );
 };
 
 const controlElement = document.getElementById('tempcontrol');
